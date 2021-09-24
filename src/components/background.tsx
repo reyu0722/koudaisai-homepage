8a64ec9b784e3e35293c71dbd2fe6d66f2a79b19
reyu <ka15sugar@gmail.com>
2021-09-25 01:12:29 +0900

    add alias setting

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,10 +1,10 @@
 const Background: FC = () => {
   return (
 		<picture>
-			<source type="image/avif" srcSet="/src/assets/background/bg-w1920.avif" />
-      <source type="image/webp" srcSet="/src/assets/background/bg-w1920.webp" />
+			<source type="image/avif" srcSet="/@/assets/background/bg-w1920.avif" />
+      <source type="image/webp" srcSet="/@/assets/background/bg-w1920.webp" />
       <img
-        src="/src/assets/background/bg-w1920.jpg"
+        src="/@/assets/background/bg-w1920.jpg"
         className="absolute top-0 z-0 w-full bgimage"
       />
     </picture>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import preact from '@preact/preset-vite'
 import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '/@': resolve(__dirname, 'src').replace(/\\/g, '/')
+    }
+  },
   plugins: [preact(), WindiCSS()]
 })
