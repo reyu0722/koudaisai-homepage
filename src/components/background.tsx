14853eaccfd606e6c51289ef9cf264eb8db184df
reyu <ka15sugar@gmail.com>
2021-09-25 01:15:37 +0900

    fix image src

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,10 +1,14 @@
+import bg_avif from '/@/assets/background/bg-w1920.avif'
+import bg_webp from '/@/assets/background/bg-w1920.webp'
+import bg_jpg from '/@/assets/background/bg-w1920.jpg'
+
 const Background: FC = () => {
   return (
-		<picture>
-			<source type="image/avif" srcSet="/@/assets/background/bg-w1920.avif" />
-      <source type="image/webp" srcSet="/@/assets/background/bg-w1920.webp" />
+    <picture>
+      <source type="image/avif" srcSet={bg_avif} />
+      <source type="image/webp" srcSet={bg_webp} />
       <img
-        src="/@/assets/background/bg-w1920.jpg"
+        src={bg_jpg}
         className="absolute top-0 z-0 w-full bgimage"
       />
     </picture>
