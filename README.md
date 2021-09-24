47d1c074e6536878f88919a4e5205c191f57945a
reyu <ka15sugar@gmail.com>
2021-09-24 17:25:38 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# 工大祭特設ページ
+
+preact + WindiCSS
+
+## 開発
+
+```
+npm i
+npm run dev
+```
---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+
+
+export function App(): JSX.Element {
+  return (
+    <>
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          className="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn Preact
+        </a>
+      </p>
+    </>
+  )
+}
