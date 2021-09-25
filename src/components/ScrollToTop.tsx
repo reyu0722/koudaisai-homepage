04c85567ddad38ca1f7fd5fbd845dfbc251473fe
reyu <ka15sugar@gmail.com>
2021-09-26 08:07:46 +0900

    use wouter

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'preact/hooks'
+import { useLocation } from 'wouter'
+
+const ScrollToTop: FC = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { pathname } = useLocation() as any
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
+export default ScrollToTop
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,17 @@
-import Router_ from 'preact-router'
+import { Route } from 'wouter'
 import App from '/@/pages/App'
 import Games from '/@/pages/Games'
 
 const Router: FC = () => {
   return (
-    <Router_>
-      <App path="/" />
-      <Games path="/games" />
-    </Router_>
+    <>
+      <Route path="/">
+        <App />
+      </Route>
+      <Route path="/games">
+        <Games />
+      </Route>
+    </>
   )
 }
 
