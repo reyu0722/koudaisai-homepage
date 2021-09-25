a1da0f33329bbf8a2b3b1f47d8cf8c6cd8ca99ca
reyu <ka15sugar@gmail.com>
2021-09-26 01:23:26 +0900

    add animation

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -24,6 +24,8 @@ const getCubeDeg = (offset: number): number => {
 const Background: FC = () => {
   const [offset, setOffset] = useState(0)
 
+  const [changed, setChanged] = useState(false)
+
   useEffect(() => {
     window.onscroll = () => {
       setOffset(window.pageYOffset / window.innerWidth)
@@ -33,20 +35,33 @@ const Background: FC = () => {
     }
   }, [])
 
+  useEffect(() => {
+    if (offset > 0.65) {
+      setChanged(true)
+    }
+  }, [offset])
+
   return (
     <>
-      <IllustNoCube className="absolute top-0 z-0 w-full bgimage fade-in-illust" />
+      {!changed ? (
+        <>
+          <IllustNoCube className="absolute top-0 z-0 w-full gradation fade-in-illust" />
+          <Cube
+            className="absolute overflow-x-hidden fade-in-illust"
+            style={`
+              width: ${getCubeWidth(offset)}rem;
+              left: ${getCubeX(offset)}rem;
+              top: ${getCubeY(offset)}rem;
+              transform: scale(1) rotate(${getCubeDeg(offset)}deg);
+            `}
+          />
+        </>
+      ) : (
+        <Illust className="absolute top-0 z-0 w-full gradation" />
+      )}
+
       <WoG className="absolute top-52 left-28 w-180 fade-in-wog" />
       <WoGBefore className="absolute top-52 left-28 w-180" />
-      <Cube
-        className="absolute overflow-x-hidden fade-in-illust"
-        style={`
-          width: ${getCubeWidth(offset)}rem;
-          left: ${getCubeX(offset)}rem;
-          top: ${getCubeY(offset)}rem;
-          transform: scale(1) rotate(${getCubeDeg(offset)}deg);
-        `}
-      />
     </>
   )
 }
