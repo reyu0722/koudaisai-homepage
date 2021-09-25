9ffe0b43aaa333dc01d9ccb1aadcaa7ee5ee2f6f
reyu <ka15sugar@gmail.com>
2021-09-26 08:09:36 +0900

    add header links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import HeaderLogo from '/@/assets/images/header-logo'
+import { Link } from 'wouter'
 
 type Props = {
   className?: string
@@ -7,8 +8,17 @@ type Props = {
 const Header: FC<Props> = ({ className }) => {
   return (
     <div
-      className={`flex items-center top-0 sticky h-20 bg-background z-10 ${className}`}>
+      className={`flex items-center top-0 sticky h-20 bg-background z-100 ${className}`}>
       <HeaderLogo className="ml-6 h-6" />
+      <div className="ml-auto mr-24">
+        これは仮置き →
+        <Link href="/" className="text-white text-xl mx-8">
+          Home
+        </Link>
+        <Link href="/games" className="text-white text-xl mx-8">
+          Games
+        </Link>
+      </div>
     </div>
   )
 }
