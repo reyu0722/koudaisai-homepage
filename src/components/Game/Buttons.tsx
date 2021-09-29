f858e1f626c9a8f856919a25c127b94863418f7e
reyu <ka15sugar@gmail.com>
2021-09-29 09:01:59 +0900

    add hover animation

--- src/components/Game/Buttons.tsx
+++ src/components/Game/Buttons.tsx
@@ -1,6 +1,8 @@
 import { useSetModalState } from '/@/store/Modal'
 import playButton from '/@/assets/buttons/playnow.svg'
+import playButtonHover from '/@/assets/buttons/playnow-hover.svg'
 import moreButton from '/@/assets/buttons/more.svg'
+import moreButtonHover from '/@/assets/buttons/more-hover.svg'
 
 const Buttons: FC = () => {
   const setModalState = useSetModalState()
@@ -16,9 +18,18 @@ const Buttons: FC = () => {
     })
 
   return (
-    <div className="flex justify-between">
-      <img src={playButton} />
-      <img src={moreButton} onClick={openModal} />
+    <div className="flex justify-between h-18">
+      <img
+        src={playButton}
+        onMouseOver={e => (e.currentTarget.src = playButtonHover)}
+        onMouseOut={e => (e.currentTarget.src = playButton)}
+      />
+      <img
+        src={moreButton}
+        onMouseOver={e => (e.currentTarget.src = moreButtonHover)}
+        onMouseOut={e => (e.currentTarget.src = moreButton)}
+        onClick={openModal}
+      />
     </div>
   )
 }
