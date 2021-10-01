a8023beaba70f3a6c64d7f9ee0fe907560fe0eb2
reyu <ka15sugar@gmail.com>
2021-10-01 11:22:04 +0900

    blur background

--- src/components/Modal/Background.tsx
+++ src/components/Modal/Background.tsx
@@ -13,10 +13,9 @@ const Background: FC = () => {
       }
     })
   return (
-    <div
-      className="fixed z-20 w-full h-full bg-black opacity-50"
-      onClick={closeModal}
-    />
+    <div className="fixed z-20 w-full h-full backdrop-filter backdrop-blur-sm">
+      <div className="w-full h-full bg-black opacity-50" onClick={closeModal} />
+    </div>
   )
 }
 
