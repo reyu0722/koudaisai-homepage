55f9c3eee96ecfd5f0ebbc91a5f913a934eedd2b
reyu <ka15sugar@gmail.com>
2021-10-02 08:39:23 +0900

    fix style

--- src/components/Modal/Body.tsx
+++ src/components/Modal/Body.tsx
@@ -5,11 +5,11 @@ import Decorations from './Decorations'
 
 const Body: FC = () => {
   return (
-    <div className="fixed inset-0 z-30 m-auto text-black rounded-6xl bg-[#E9FAFF] max-w-[80vw] max-h-[80vh]">
-      <div className="overflow-hidden relative w-full h-full">
+    <div className="overflow-hidden fixed inset-0 z-30 m-auto text-black rounded-6xl bg-[#E9FAFF] max-w-[80vw] max-h-[80vh]">
+      <div className="relative w-full h-full">
         <Decorations />
         <div className="relative z-40 py-20 px-8 md:px-20 w-full h-full">
-          <div className="text-4xl font-bold">あそびかた</div>
+          <div className="text-4xl font-medium">あそびかた</div>
           <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-6 md:grid-rows-4 gap-6 md:gap-12 justify-between mt-6 md:mt-12 h-[calc(100%-5rem)]">
             <div className="row-span-3 md:row-span-4">
               ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ
--- src/components/Modal/Buttons.tsx
+++ src/components/Modal/Buttons.tsx
@@ -4,8 +4,8 @@ import playDark from '/@/assets/buttons/play-dark.svg'
 const Buttons: FC = () => {
   return (
     <div className="flex justify-between w-full h-full">
-      <img src={blog} className="max-h-full" />
-      <img src={playDark} className="max-h-full" />
+      <img src={blog} className="flex-1 w-0 max-h-full" />
+      <img src={playDark} className="flex-1 w-0 max-h-full" />
     </div>
   )
 }
