cc854b89ca8dc3e3c9d2965251606224dc3ccd8b
reyu <ka15sugar@gmail.com>
2021-10-03 01:39:09 +0900

    fix animation

--- src/components/Game/Animation/index.tsx
+++ src/components/Game/Animation/index.tsx
@@ -7,17 +7,16 @@ type Props = {
 
 const Animation: FC<Props> = ({ visible }) => {
   return (
-    <div className="relative h-60 ml-[60%]">
-      <img src={bg} className="absolute top-0 right-60" />
-      <img
-        src={circle}
-        className="absolute light"
-        style={
-          visible
-            ? `top:7.5rem; right:22.5rem; `
-            : `top:-0.75rem;right:14.25rem`
-        }
-      />
+    // eslint-disable-next-line tailwindcss/no-custom-classname
+    <div className="relative left-3/5 h-60 max-w-1/4">
+      <div className="absolute top-0 w-auto">
+        <img src={bg} className="top-0" />
+        <img
+          src={circle}
+          className="absolute w-2/5 light"
+          style={visible ? `top:70%;right:70%; ` : `top:-8%;right:-8%`}
+        />
+      </div>
     </div>
   )
 }
