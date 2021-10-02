9776aa3c8964926bf8a6b4c11027c14bac9b2cf8
reyu <ka15sugar@gmail.com>
2021-10-02 23:17:46 +0900

    fix css

--- src/components/Modal/Buttons.tsx
+++ src/components/Modal/Buttons.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable tailwindcss/no-custom-classname */
 import blog from '/@/assets/buttons/blog.svg'
 import playDark from '/@/assets/buttons/play-dark.svg'
 
 const Buttons: FC = () => {
   return (
     <div className="flex justify-between w-full h-full">
-      <img src={blog} className="flex-1 w-0 max-h-full" />
-      <img src={playDark} className="flex-1 w-0 max-h-full" />
+      <img src={blog} className="flex-1 w-0 max-h-full max-w-60" />
+      <img src={playDark} className="flex-1 w-0 max-h-full max-w-60" />
     </div>
   )
 }
--- src/components/Modal/Decorations/index.tsx
+++ src/components/Modal/Decorations/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable tailwindcss/no-custom-classname */
 import meshTop from './mesh-top.svg'
 import meshBottom from './mesh-bottom.svg'
 import titlebg from './titlebg.svg'
@@ -5,8 +6,8 @@ import titlebg from './titlebg.svg'
 const Decorations: FC = () => {
   return (
     <>
-      <img src={meshTop} className="absolute top-0 right-0" />
-      <img src={meshBottom} className="absolute bottom-0 left-0" />
+      <img src={meshTop} className="absolute top-0 left-[55%] min-w-[45%]" />
+      <img src={meshBottom} className="absolute bottom-0 right-[55%] min-w-[45%]" />
       {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
       <img src={titlebg} className="absolute top-16 w-52 left-18" />
       {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
