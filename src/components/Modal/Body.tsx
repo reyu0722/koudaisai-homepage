5c07d77514a5e9d278e71229a9865b1c1d6f1811
reyu <ka15sugar@gmail.com>
2021-10-03 13:38:06 +0900

    fix

--- src/components/Modal/Body.tsx
+++ src/components/Modal/Body.tsx
@@ -9,18 +9,16 @@ const Body: FC = () => {
 
   return (
     <div className="overflow-hidden fixed inset-0 z-30 m-auto text-black rounded-6xl bg-[#E9FAFF] max-w-[80vw] max-h-[80vh]">
-      <div className="relative w-full h-full">
-        <Decorations />
-        <div className="relative z-40 py-20 px-8 lg:px-20 space-y-6 lg:space-y-12 w-full h-full">
-          <div className="<lg:pl-12 text-4xl font-medium">あそびかた</div>
-          <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-6 lg:grid-rows-4 gap-6 lg:gap-12 justify-between h-[calc(100%-5rem)]">
-            <div className="row-span-3 lg:row-span-4">{howToPlay}</div>
-            <div className="row-span-2 lg:row-span-3 row-start-1">
-              <Carousel images={imgUrls} />
-            </div>
-            <div className="row-span-1">
-              <Buttons blogUrl={blogUrl} gameUrl={gameUrl} />
-            </div>
+      <Decorations />
+      <div className="relative z-40 py-20 px-8 lg:px-20 space-y-6 lg:space-y-12 w-full h-full">
+        <div className="<lg:pl-12 text-4xl font-medium">あそびかた</div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 grid-rows-6 lg:grid-rows-4 gap-6 lg:gap-12 justify-between h-[calc(100%-5rem)]">
+          <div className="row-span-3 lg:row-span-4">{howToPlay}</div>
+          <div className="row-span-2 lg:row-span-3 row-start-1">
+            <Carousel images={imgUrls} />
+          </div>
+          <div className="row-span-1">
+            <Buttons blogUrl={blogUrl} gameUrl={gameUrl} />
           </div>
         </div>
       </div>
