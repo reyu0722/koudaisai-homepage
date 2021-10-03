fdc714faea4004069685cada832499f3db1e3ce1
reyu <ka15sugar@gmail.com>
2021-10-04 01:43:19 +0900

    fix font size

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -15,14 +15,14 @@ const App: FC = () => {
         {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
         <div className="py-[80vw]"> </div>
 
-        <div className="overflow-hidden">
+        <div className="overflow-hidden text-lg md:text-2xl">
           <div className="mx-12 mt-24 md:mr-48 md:ml-24">
             <div className="ml-12 text-xl">About</div>
             {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
             <div className="mt-12 text-6xl mb-18 text-mypurple">
               World of Games
             </div>
-            <div className="text-lg">
+            <div>
               ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ。
             </div>
           </div>
