f4d6004133dae03007c71e2050af80c47aa4ad09
reyu <ka15sugar@gmail.com>
2021-10-04 02:59:35 +0900

    modalを開いているときはスクロール禁止

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,9 +1,18 @@
 import Background from './Background'
 import Body from './Body'
 import { useModalState } from '/@/store/Modal'
+import { useEffect } from 'preact/hooks'
 
 const Modal: FC = () => {
   const { isOpen } = useModalState()
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }, [isOpen])
 
   if (isOpen)
     return (
