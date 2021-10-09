dde0ad32ad0f3cb8f0e01f9d2f610b756eb748fe
logica <84759675+logica0419@users.noreply.github.com>
2021-10-09 21:49:08 +0900

    ヘッダーの改変 - of小さくしたかったので小さくしたうえで全体のバランスとった - 欲張りなのでちょっと大きくした

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ const Header: FC<Props> = ({ className }) => {
   return (
     <div
       className={`absolute flex top-0 z-10 justify-between items-center pr-12 pl-6 w-full h-20 bg-background ${className}`}>
-      <HeaderLogo className="h-3 md:h-6" />
+      <HeaderLogo className="h-3 md:h-8" />
     </div>
   )
 }
