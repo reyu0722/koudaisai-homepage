77d8ebf3c85ecfd641409449a604ace99f0d41a6
reyu <ka15sugar@gmail.com>
2021-10-10 00:13:44 +0900

    fix mobile header height

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,8 +7,8 @@ type Props = {
 const Header: FC<Props> = ({ className }) => {
   return (
     <div
-      className={`absolute flex top-0 z-10 justify-between items-center pr-12 pl-6 w-full h-20 bg-background ${className}`}>
-      <HeaderLogo className="h-3 md:h-8" />
+      className={`absolute flex top-0 z-10 justify-between items-center pr-12 pl-6 w-full h-12 md:h-20 bg-background ${className}`}>
+      <HeaderLogo className="h-4 md:h-8" />
     </div>
   )
 }
