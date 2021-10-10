9a40d15d6b89fa9d2bbc7fb4adffbc6293e5ef94
reyu <ka15sugar@gmail.com>
2021-10-10 16:11:44 +0900

    add click event

--- src/components/Modal/Buttons.tsx
+++ src/components/Modal/Buttons.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'preact/hooks'
 
 import Blog from '/@/components/Button/Blog'
 import PlayNowDark from '/@/components/Button/PlayNowDark'
+import { useSetModalState } from '/@/store/Modal'
 
 type Props = {
   blogUrl?: string
@@ -15,12 +16,15 @@ const Buttons: FC<Props> = ({ blogUrl, gameUrl }) => {
     setWidth(document.body.clientWidth)
   }, [])
 
+  const setModalState = useSetModalState()
+  const closeModal = () => setModalState(e => ({ ...e, isOpen: false }))
+
   return (
     <div className="flex <md:flex-col gap-2 md:justify-between <md:items-center w-full h-full">
       {blogUrl && <Blog url={blogUrl} />}
       <PlayNowDark url={gameUrl} />
       {width < 640 && (
-        <div className="w-48 sm:w-60 <md:h-12 h-16">
+        <div className="w-48 sm:w-60 <md:h-12 h-16" onClick={closeModal}>
           <div className="flex justify-evenly items-center py-2 w-full h-full">
             <p className="font-bold leading-none text-left align-top whitespace-nowrap text-[18px] sm:text-[24px] font-noto text-[#e3040]">
               CLOSE
