62a1fa7e0e8de6823644f051232a6aca3c61a51d
reyu <ka15sugar@gmail.com>
2021-10-19 14:06:47 +0900

    add trap logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import HeaderLogo from '/@/assets/images/header-logo'
+import traPLogo from '/@/assets/logo/trap-short.svg'
 
 type Props = {
   className?: string
@@ -9,6 +10,7 @@ const Header: FC<Props> = ({ className }) => {
     <div
       className={`absolute flex top-0 z-10 justify-between items-center pr-12 pl-6 w-full h-12 md:h-20 bg-background ${className}`}>
       <HeaderLogo className="h-4 md:h-8" />
+      <img src={traPLogo} className="h-4 md:h-8" />
     </div>
   )
 }
